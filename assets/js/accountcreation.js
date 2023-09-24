1a5407b2590d77153413daa0606463cc39e55339
Pritish Rajpurohit <workforpritish@gmail.com>
2023-09-24 18:45:42 +0530

    Initial commit

---
+++ assets/js/accountcreation.js
@@ -0,0 +1,7 @@
+function redirect() {
+        // Define the relative path to the destination page in the same folder
+        var redirectTo = "register.html"; // Replace with the actual filename
+
+        // Perform the redirect
+        window.location.href = redirectTo;
+    }
